f1c681d6103b6f625439fcd422e9ad74ff528a2f
SvenAventador <asumilkin67@gmail.com>
2023-02-18 16:52:04 +0300

    Initial commit

---
+++ HorseClubProject/Models/DurationViewModels.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+using HorseClubLibrary.Model.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace HorseClubProject.Models;
+
+public class DurationViewModels
+{
+    [DisplayName("Duration")] public int DurationId { get; set; }
+
+    public List<DurationTours> ListofDuration { get; set; }
+}
